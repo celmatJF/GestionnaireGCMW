f01c6ee3eb98b03b8c742a2cbd5957d6ae82177b
137273898423 <william.lacoste@lyceejulesfil.fr>
2026-01-29 16:43:03 +0100

    Réduction du header de ExportFile.cpp + Ajout commentaire

--- Code/ExportFile.cpp
+++ Code/ExportFile.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
+#include <fstream>
 #include <windows.h>
 #include <clocale>
-#include "AddStudent.h"
-#include "liste.h"
-#include "moyenne.h"
-#include "searchElv.h"
-#include "Best.h"
 #include "struct.hpp"
-#include <fstream>
+#include "ExportFile.h"
 using namespace std;
 
-string const FichierClasse = "ListeClasse.csv";
+string const FichierClasse = "ListeClasse.csv"; // Création du nom du fichier
 
 void ExportFile(eleve tab[30])
 {
-    ofstream AccesFichier(FichierClasse.c_str());
+    /* Usage: Exporter la liste des élèves ainsi 
+    que leurs notes dans un fichier .csv
 
-    if(AccesFichier)
+    Entrée: Variable tab[30] de type eleve 
+    (venant de la structure du struct.hpp) 
+    
+    Sortie: Création d'un fichier .csv
+    */
+
+    ofstream AccesFichier(FichierClasse.c_str()); // Création du fichier
+
+    if(AccesFichier) // Vérifie si le fichier a bien été créer
     {
-        AccesFichier << "Nom,Note" << endl;
-        for(int i = 0; i < 30; i++)
+        AccesFichier << "Nom,Note" << endl; // Ecris la premiere ligne
+        for(int i = 0; i < 30; i++) // Boucle pour les trentes élèves
         {
-            if(tab[i].nom != "")
+            if(tab[i].nom != "") // Vérifie si la case numéro i n'est pas vide
             {
-                AccesFichier << tab[i].nom << ',' << tab[i].note << endl; 
+                AccesFichier << tab[i].nom << ',' << tab[i].note << endl; // Rajoute la ligne du nom et de la note de l'èleve au fichier .csv
             }
         }
-        cout << BLUE << "Le fichier a été exporter avec succès" << endl;
+        cout << BLUE << "Le fichier a été exporter avec succès" << endl; // Affichage confirmations de l'export
     } 
     else
     {
-        cout << BLUE << "Le fichier n'a pas été exporter" << endl;
+        cout << BLUE << "Le fichier n'a pas été exporter" << endl; // Affichage mauvais export
     }
-    cout << RESET;
+    cout << RESET; // Fin
 }
